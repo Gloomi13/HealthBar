7b7e81e306e5ecce846b326e6e1bd2ba70f16f11
Gloomi13 <Gloomi13@mail.ru>
2022-07-03 22:22:45 +1000

    3

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -8,13 +8,28 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Player player;
     [SerializeField] private Slider slider;
     
-    public void ChangeHealthBar()
+    public void ChangeHealthBar(float number)
     {
-        slider.value=player.Health;
+        StartCoroutine(SmoothlyChangeHealthBar(number));
     }
 
     private void Start()
     {
         slider.maxValue=player.MaxHealth;
+        slider.value=player.Health;
+    }
+
+        private IEnumerator SmoothlyChangeHealthBar(float number)
+    {
+        var waitForSeconds = new WaitForSeconds(0.05f);
+        int speedChange = 10;
+
+        for (int i = 0; i < speedChange; i++)
+        {
+            slider.value = Mathf.MoveTowards(slider.value, slider.maxValue, number /speedChange);
+            slider.value = Mathf.Clamp(slider.value, 0,slider.maxValue);
+
+            yield return waitForSeconds;
+        }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,34 +4,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private HealthBar healthBar;
-    public float Health { get; private set; }
 
+    public float Health { get; private set; }
     public float MaxHealth { get; private set; }
 
-    public void ChangeHealPlayer(float number)
+    public void DoHealPlayer(float Heal)
     {
-      StartCoroutine(ChangeHealth(number));
+      Health+=Heal;
+      Health = Mathf.Clamp(Health, 0,MaxHealth);
+      healthBar.ChangeHealthBar(Heal);
+    }
+
+    public void DoDamagePlayer(float Damage)
+    {
+      Health-=Damage;
+      Health = Mathf.Clamp(Health, 0,MaxHealth);
+      healthBar.ChangeHealthBar(-Damage);
     }
 
     private void Awake()
     {
         Health = 80;
         MaxHealth = 100;
-        healthBar.ChangeHealthBar();
-    }
-
-    private IEnumerator ChangeHealth(float number)
-    {
-        var waitForSeconds = new WaitForSeconds(0.05f);
-        int speedChange = 10;
-
-        for (int i = 0; i < speedChange; i++)
-        {
-            Health = Mathf.MoveTowards(Health, MaxHealth, number / speedChange);
-            Health = Mathf.Clamp(Health, 0,MaxHealth);
-            healthBar.ChangeHealthBar();
-
-            yield return waitForSeconds;
-        }
     }
 }
