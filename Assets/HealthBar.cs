9810215e4972eb113c713ecbfb273b7421651782
Gloomi13 <Gloomi13@mail.ru>
2022-07-04 12:41:43 +1000

    6

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -7,19 +7,22 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] private Slider _slider;
-    
-    public void ChangeHealthBar(float number)
-    {
-        StartCoroutine(SmoothlyChangeHealthBar(number));
-    }
 
+        public void Change()
+    {
+        if( _slider.value!=_player.Health)
+        { 
+            StartCoroutine(SmoothlyChange(_player.Health -_slider.value));
+        }
+    }
+    
     private void Start()
     {
         _slider.maxValue=_player.MaxHealth;
         _slider.value=_player.Health;
     }
 
-        private IEnumerator SmoothlyChangeHealthBar(float number)
+        private IEnumerator SmoothlyChange(float number)
     {
         var waitForSeconds = new WaitForSeconds(0.05f);
         int speedChange = 10;
